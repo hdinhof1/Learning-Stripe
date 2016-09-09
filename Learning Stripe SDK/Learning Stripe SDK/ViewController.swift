7649b9ad20f5eab3b78a1d2b160fd2ff5ddef460
hdinhof1 <hdinhof1@binghamton.edu>
2016-09-08 21:40:39 -0400

    First commit

---
+++ Learning Stripe SDK/Learning Stripe SDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Stripe SDK
+//
+//  Created by Henry Dinhofer on 9/8/16.
+//  Copyright © 2016 Henry Dinhofer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
